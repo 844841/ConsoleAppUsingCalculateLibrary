dd0741f427d92e1e019a76b48f76e830ba50337b
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:57:44 +0530

    Initial commit

---
+++ ConsoleAppUsingCalculateLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibCalculation;
+
+namespace ConsoleAppUsingCalculateLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation cl = new Calculation();
+            cl.No1 = 404;
+            cl.No2 = 50;
+            Console.WriteLine(cl.Add());
+            Console.WriteLine(cl.Difference());
+            Console.WriteLine(cl.Multiply());
+            Console.WriteLine(cl.Division());
+
+        }
+    }
+}
